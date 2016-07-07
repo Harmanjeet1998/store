d217c545cbe47871644abad5fbc0b3d323aeadf8
Harmanjeet1998 <nokwalharman@gmail.com>
2016-07-07 12:22:48 +0530

    set background

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require dataTables/jquery.dataTables
+//= require dataTables/bootstrap/2/jquery.dataTables.bootstrap
+//= require dataTables/jquery.dataTables
 //= require turbolinks
 //= require_tree .
 
--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,3 +1,4 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+onclick="this.style.margin='4% 20% 80% 80%';"
